b3978ed1339a1029331af232bc566aa33fdd5de3
Dylan Gageot <gageot.dylan@gmail.com>
2020-08-06 15:43:19 +0200

    Add forward method to push_server

--- push_server.h
+++ push_server.h
@@ -5,6 +5,9 @@
 #ifndef __PUSH_SERVER_H__
 #define __PUSH_SERVER_H__
 
+#include "tetris_feature.h"
+#include "socket.h"
+
 namespace TETRiS {
     /**
      * \brief TETRiS Push Server.
@@ -13,7 +16,42 @@ namespace TETRiS {
      * logic for redistributing message to the corresponding TETRiS feature.
      */
     class PushServer {
+    public:
+        /**
+         * \brief Builds a PushServer.
+         *
+         * Behind the scene, a thread is launched to listen on the push server.
+         *
+         * \param [in] socket_path Path to the socket to run the server on.
+         */
+        explicit PushServer(const std::string &socket_path);
 
+        /**
+         * \brief Adds a TETRiS feature with its id to the subscribers map.
+         * \param feature_id Feature ID.
+         * \param feature Pointer to feature.
+         */
+        void add_subscriber(const FeatureID &feature_id, Feature *feature);
+
+        /**
+         * \brief Forwards the push request to the specified feature.
+         * \param request Request to forward.
+         * \return Forwarded response.
+         */
+        PushResponse forward(const PushRequest &request) const;
+
+    private:
+        /**
+         * \brief Thread listener.
+         * \return None.
+         */
+        static void *listening(void *args);
+
+        /// \brief Push server socket.
+        Socket server;
+
+        /// \brief Subscribers.
+        std::map<FeatureID, Feature *> subscribers;
     };
 }
 
