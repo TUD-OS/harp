ec972180a5e90e09eb660fb9c7ed881d5887b675
Dylan Gageot <gageot.dylan@gmail.com>
2020-08-07 09:38:50 +0200

    Separate implementations from header for path_util and util

---
+++ util.cc
@@ -0,0 +1,20 @@
+//
+// Created by dylan on 07/08/2020.
+//
+
+#include "util.h"
+
+void util::make_fd_non_blocking(int fd)
+{
+    int flags;
+
+    flags = ::fcntl(fd, F_GETFL, 0);
+    if (flags == -1) {
+        throw std::runtime_error{"Failed to get flags of the socket."};
+    } else {
+        flags |= O_NONBLOCK;
+        if (::fcntl(fd, F_SETFL, flags) == -1) {
+            throw std::runtime_error{"Failed to set flags on socket."};
+        }
+    }
+}
--- util.h
+++ util.h
@@ -11,20 +11,7 @@
 
 namespace util {
 
-void make_fd_non_blocking(int fd)
-{
-    int flags;
-
-    flags = ::fcntl(fd, F_GETFL, 0);
-    if (flags == -1) {
-        throw std::runtime_error{"Failed to get flags of the socket."};
-    } else {
-        flags |= O_NONBLOCK;
-        if (::fcntl(fd, F_SETFL, flags) == -1) {
-            throw std::runtime_error{"Failed to set flags on socket."};
-        }
-    }
-}
+void make_fd_non_blocking(int fd);
 
 } /* namespace util */
 
