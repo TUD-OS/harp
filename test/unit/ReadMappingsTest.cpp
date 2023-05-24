e75bb109cac5f068d6b94b60ef0d661a726defdc
Robert Khasanov <robert.khasanov@tu-dresden.de>
2023-05-24 20:21:11 +0200

    test: added test for MappingReader::read_mapping_directory()

---
+++ test/unit/ReadMappingsTest.cpp
@@ -0,0 +1,23 @@
+#include <gtest/gtest.h>
+
+#include "server/mapping_reader.h"
+
+// This test checks the functionality of the read_mapping_directory function
+TEST(MappingReaderTest, ReadMappingDirectoryTest) {
+  // Define the directory containing the test mapping data
+  std::string test_directory = "../mappings";
+
+  // Call the function to read mappings from the test directory
+  auto app_mappings = MappingReader::read_mapping_directory(test_directory);
+
+  // Check the number of applications for which mappings have been read
+  EXPECT_EQ(app_mappings.size(), 3);
+
+  // Check the number of mappings read for the 'mandelbrot' application
+  EXPECT_EQ(app_mappings.count("mandelbrot"), 1);
+  EXPECT_EQ(app_mappings["mandelbrot"].size(), 3);
+
+  // Check that mappings have been read for the 'htop' and 'jpeg' applications
+  EXPECT_EQ(app_mappings.count("htop"), 1);
+  EXPECT_EQ(app_mappings.count("jpeg"), 1);
+}
