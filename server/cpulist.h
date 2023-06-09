7555eb9fb0e15c0c56a44a594b61288ffbfa20f6
Robert Khasanov <robert.khasanov@tu-dresden.de>
2023-06-09 19:22:41 +0200

    server: documenenting and formatting cpulist.h and equivalence.h

--- server/cpulist.h
+++ server/cpulist.h
@@ -3,144 +3,110 @@
 
 #pragma once
 
-
-#include <algorithm>
-#include <initializer_list>
-#include <map>
-#include <vector>
-
 #include <sched.h>
 
+#include <initializer_list>
+#include <vector>
 
-class CPUList
-{
-   private:
-    cpu_set_t    _cpus;
+/**
+ * \class CPUList
+ * \brief Represents a set of CPUs and provides operations for manipulating CPU
+ * sets.
+ *
+ * This class is a wrapper around cpu_set_t, providing a more intuitive
+ * interface for creating and manipulating CPU sets.
+ */
+class CPUList {
+ private:
+  cpu_set_t _cpus;
 
-   public:
-    CPUList()
-    {
-        CPU_ZERO(&_cpus);
-    }
+ public:
+  CPUList() { CPU_ZERO(&_cpus); }
 
-    template <template<typename> class Container>
-    CPUList(const Container<int>& cpus) :
-        CPUList{}
-    {
-        for (const auto c : cpus)
-            CPU_SET(c, &_cpus);
-    }
+  template <template <typename> class Container>
+  CPUList(const Container<int>& cpus) : CPUList{} {
+    for (const auto c : cpus) CPU_SET(c, &_cpus);
+  }
 
-    CPUList(const std::initializer_list<int>& cpus) :
-        CPUList{}
-    {
-        for (const auto c : cpus)
-            CPU_SET(c, &_cpus);
-    }
+  CPUList(const std::initializer_list<int>& cpus) : CPUList{} {
+    for (const auto c : cpus) CPU_SET(c, &_cpus);
+  }
 
-    explicit CPUList(cpu_set_t cpus) :
-        _cpus{cpus}
-    {}
+  explicit CPUList(cpu_set_t cpus) : _cpus{cpus} {}
 
-    CPUList(const CPUList& o) = default;
-    CPUList(CPUList&& o) = default;
+  CPUList(const CPUList& o) = default;
+  CPUList(CPUList&& o) = default;
 
-    CPUList& operator=(const CPUList& o) = default;
-    CPUList& operator=(CPUList&& o) = default;
+  CPUList& operator=(const CPUList& o) = default;
+  CPUList& operator=(CPUList&& o) = default;
 
-    CPUList& operator=(cpu_set_t cpus)
-    {
-        _cpus = cpus;
+  CPUList& operator=(cpu_set_t cpus) {
+    _cpus = cpus;
 
-        return *this;
-    }
+    return *this;
+  }
 
-    bool operator==(const CPUList& o) const
-    {
-        return CPU_EQUAL(&_cpus, &o._cpus);
-    }
+  bool operator==(const CPUList& o) const {
+    return CPU_EQUAL(&_cpus, &o._cpus);
+  }
 
-    bool operator!=(const CPUList& o) const
-    {
-        return !(*this == o);
-    }
+  bool operator!=(const CPUList& o) const { return !(*this == o); }
 
-    CPUList operator&(const CPUList& o) const
-    {
-        CPUList tmp{};
+  CPUList operator&(const CPUList& o) const {
+    CPUList tmp{};
 
-        CPU_AND(&tmp._cpus, &_cpus, &o._cpus);
+    CPU_AND(&tmp._cpus, &_cpus, &o._cpus);
 
-        return tmp;
-    }
+    return tmp;
+  }
 
-    CPUList& operator&=(const CPUList& o)
-    {
-        CPU_AND(&_cpus, &_cpus, &o._cpus);
+  CPUList& operator&=(const CPUList& o) {
+    CPU_AND(&_cpus, &_cpus, &o._cpus);
 
-        return *this;
-    }
+    return *this;
+  }
 
-    CPUList operator|(const CPUList& o) const
-    {
-        CPUList tmp{};
+  CPUList operator|(const CPUList& o) const {
+    CPUList tmp{};
 
-        CPU_OR(&tmp._cpus, &_cpus, &o._cpus);
+    CPU_OR(&tmp._cpus, &_cpus, &o._cpus);
 
-        return tmp;
-    }
+    return tmp;
+  }
 
-    CPUList operator|=(const CPUList& o)
-    {
-        CPU_OR(&_cpus, &_cpus, &o._cpus);
+  CPUList operator|=(const CPUList& o) {
+    CPU_OR(&_cpus, &_cpus, &o._cpus);
 
-        return *this;
-    }
+    return *this;
+  }
 
-    void set(int cpu_nr)
-    {
-        CPU_SET(cpu_nr, &_cpus);
-    }
+  void set(int cpu_nr) { CPU_SET(cpu_nr, &_cpus); }
 
-    void clear(int cpu_nr)
-    {
-        CPU_CLR(cpu_nr, &_cpus);
-    }
+  void clear(int cpu_nr) { CPU_CLR(cpu_nr, &_cpus); }
 
-    void zero()
-    {
-        CPU_ZERO(&_cpus);
-    }
+  void zero() { CPU_ZERO(&_cpus); }
 
-    bool overlaps_with(const CPUList& o) const
-    {
-        cpu_set_t tmp;
+  bool overlaps_with(const CPUList& o) const {
+    cpu_set_t tmp;
 
-        CPU_AND(&tmp, &_cpus, &o._cpus);
+    CPU_AND(&tmp, &_cpus, &o._cpus);
 
-        return CPU_COUNT(&tmp) != 0;
-    }
+    return CPU_COUNT(&tmp) != 0;
+  }
 
-    int nr_cpus() const
-    {
-        return CPU_COUNT(&_cpus);
-    }
+  int nr_cpus() const { return CPU_COUNT(&_cpus); }
 
-    cpu_set_t cpu_set() const
-    {
-        return _cpus;
-    }
+  cpu_set_t cpu_set() const { return _cpus; }
 
-    std::vector<int> cpulist(int max_cpus) const
-    {
-        std::vector<int> result;
+  // Returns a list of CPU indices in the set.
+  std::vector<int> cpulist(int max_cpus) const {
+    std::vector<int> result;
 
-        for (int i = 0; i < max_cpus; ++i)
-            if (CPU_ISSET(i, &_cpus))
-                result.push_back(i);
+    for (int i = 0; i < max_cpus; ++i)
+      if (CPU_ISSET(i, &_cpus)) result.push_back(i);
 
-        return result;
-    }
+    return result;
+  }
 };
 
 #endif /* __CPULIST_H__ */
--- server/equivalence.h
+++ server/equivalence.h
@@ -3,98 +3,126 @@
 
 #pragma once
 
-
-#include "cpulist.h"
-
 #include <initializer_list>
 #include <map>
 #include <stdexcept>
 #include <string>
 #include <vector>
 
+#include "cpulist.h"
 
-class EqualCPUS
-{
-   private:
-    CPUList         _cpulist;
-    std::vector<int>    _cpus;
+/**
+ * \class EqualCPUS
+ * \brief Represents a set of CPUs that are considered equivalent.
+ *
+ * This class is used to manage and manipulate a group of CPUs
+ * that are considered equal in terms of their capabilities.
+ */
+// TODO (RK): In my opinion, the name of this class does not accurately
+// represent its semantics. This class essentially acts as a wrapper for a
+// CPUList, with an additional std::vector<int> that represents the same CPU
+// list, along with a method to convert one EqualCPUS object to another.
+// However, the actual CPU list could include non-equivalent cores in one
+// object, such as CORTEX-A7 and CORTEX-A15, which contradicts the implied
+// meaning of 'EqualCPUS'.
 
+class EqualCPUS {
+ private:
+  CPUList _cpulist;
+  std::vector<int> _cpus;
 
-    friend class Equivalence;
+  friend class Equivalence;
 
-    std::map<int, int> conversion_map(const EqualCPUS& other) const
-    {
-        if (other._cpulist == _cpulist)
-            return {};
+  // Generates a conversion map from this set to another set of equivalent CPUs.
+  std::map<int, int> conversion_map(const EqualCPUS& other) const {
+    if (other._cpulist == _cpulist) return {};
 
-        if (_cpus.size() != other._cpus.size())
-            throw std::runtime_error{"Can't generate conversion map for mapping of different equivalence classes."};
+    if (_cpus.size() != other._cpus.size())
+      throw std::runtime_error{
+          "Can't generate conversion map for mapping of different equivalence "
+          "classes."};
 
-        std::map<int, int> result;
+    std::map<int, int> result;
 
-        for (size_t i = 0; i < _cpus.size(); ++i) {
-            if (_cpus[i] != other._cpus[i])
-                result.emplace(_cpus[i], other._cpus[i]);
-        }
-
-        return result;
+    for (size_t i = 0; i < _cpus.size(); ++i) {
+      if (_cpus[i] != other._cpus[i]) result.emplace(_cpus[i], other._cpus[i]);
     }
 
-   public:
-    EqualCPUS(const std::initializer_list<int>& cpus) :
-        _cpulist{cpus}, _cpus{cpus}
-    {}
+    return result;
+  }
 
-    bool operator==(const CPUList& o) const
-    {
-        return _cpulist == o;
-    }
+ public:
+  EqualCPUS(const std::initializer_list<int>& cpus)
+      : _cpulist{cpus}, _cpus{cpus} {}
+
+  bool operator==(const CPUList& o) const { return _cpulist == o; }
 };
 
 bool operator==(const CPUList& c, const EqualCPUS& ec);
 
+/**
+ * \class Equivalence
+ * \brief Represents an equivalence class of CPU sets.
+ *
+ * This class is used to manage a collection of sets of CPUs that
+ * are considered interchangeable or equivalent in terms of their performance
+ * characteristics.
+ */
+class Equivalence {
+ private:
+  std::string _name;
+  std::vector<EqualCPUS> _equalcpus;
 
-class Equivalence
-{
-   private:
-    std::string     _name;
-    std::vector<EqualCPUS>  _equalcpus;
+ public:
+  Equivalence(const std::string& name,
+              const std::initializer_list<EqualCPUS>& equalcpulists)
+      : _name{name}, _equalcpus{equalcpulists} {}
 
-   public:
-    Equivalence(const std::string& name, const std::initializer_list<EqualCPUS>& equalcpulists) :
-        _name{name}, _equalcpus{equalcpulists}
-    {}
+  const std::string name() const { return _name; }
 
-    const std::string name() const
-    {
-        return _name;
+  // Checks if a given CPUList is part of the equivalence class.
+  bool is_in_equalence_class(const CPUList& cpulist) const {
+    for (const auto& ecpus : _equalcpus) {
+      if (cpulist == ecpus) return true;
     }
 
-    bool is_in_equalence_class(const CPUList& cpulist) const
-    {
-        for (const auto& ecpus : _equalcpus) {
-            if (cpulist == ecpus)
-                return true;
-        }
+    return false;
+  }
 
-        return false;
+  /**
+   * \brief Get a list of equivalent mappings within the equivalence class for a
+   * given CPUList.
+   *
+   * This function checks if the given CPUList is part of this equivalence
+   * class, and if so, constructs a list of conversion maps from the matching
+   * EqualCPUS instance to all other EqualCPUS instances within the equivalence
+   * class. Each conversion map consists of pairs where each key-value pair
+   * represents a CPU in the source EqualCPUS and its corresponding CPU in the
+   * target EqualCPUS.
+   *
+   * \param cpulist The CPUList to be checked and for which equivalent mappings
+   * are to be generated. \return A vector of maps where each map is a
+   * conversion map from the given CPUList to another possible CPU configuration
+   * within the equivalence class. \throws std::runtime_error if the provided
+   * CPUList does not match any of the EqualCPUS instances within this
+   * equivalence class.
+   */
+  std::vector<std::map<int, int>> equivalent_mappings(
+      const CPUList& cpulist) const {
+    for (const auto& ecpus : _equalcpus) {
+      if (cpulist == ecpus) {
+        std::vector<std::map<int, int>> result;
+
+        for (const auto& other : _equalcpus)
+          result.push_back(ecpus.conversion_map(other));
+
+        return result;
+      }
     }
 
-    std::vector<std::map<int, int>> equivalent_mappings(const CPUList& cpulist) const
-    {
-        for (const auto& ecpus : _equalcpus) {
-            if (cpulist == ecpus) {
-                std::vector<std::map<int, int>> result;
-
-                for (const auto& other : _equalcpus)
-                    result.push_back(ecpus.conversion_map(other));
-
-                return result;
-            }
-        }
-
-        throw std::runtime_error{"This mapping is not part of this equivalence class."};
-    }
+    throw std::runtime_error{
+        "This mapping is not part of this equivalence class."};
+  }
 };
 
 #endif /* __EQUIVALENCE_H__ */
