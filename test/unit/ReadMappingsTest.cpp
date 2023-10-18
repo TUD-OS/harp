d7e29d7cdb2c854d0f75e447c598faa2098df10e
Till Smejkal <till.smejkal@tu-dresden.de>
2023-10-18 10:43:45 +0200

    New mappings for the OpenMP applications on RaptorLake

--- test/unit/ReadMappingsTest.cpp
+++ test/unit/ReadMappingsTest.cpp
@@ -47,15 +47,15 @@ TEST(MappingReaderTest, ReadMappingDirectoryRaptorTest) {
   auto app_mappings = mapping_reader.read_mapping_directory(test_directory);
 
   // Check the number of applications for which mappings have been read
-  EXPECT_EQ(app_mappings.size(), 3);
+  EXPECT_EQ(app_mappings.size(), 10);
 
-  // Check the number of mappings read for the 'mandelbrot' application
-  EXPECT_EQ(app_mappings.count("ep_C"), 1);
-  EXPECT_EQ(app_mappings["ep_C"].size(), 86);
+  // Check the number of mappings read for the OpenMP applications
+  EXPECT_EQ(app_mappings.count("ep.C"), 1);
+  EXPECT_EQ(app_mappings["ep.C"].size(), 93);
 
-  EXPECT_EQ(app_mappings.count("mg_C"), 1);
-  EXPECT_EQ(app_mappings["mg_C"].size(), 48);
+  EXPECT_EQ(app_mappings.count("mg.C"), 1);
+  EXPECT_EQ(app_mappings["mg.C"].size(), 50);
 
-  EXPECT_EQ(app_mappings.count("sp_B"), 1);
-  EXPECT_EQ(app_mappings["sp_B"].size(), 164);
+  EXPECT_EQ(app_mappings.count("sp.B"), 1);
+  EXPECT_EQ(app_mappings["sp.B"].size(), 164);
 }
