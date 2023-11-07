c309b8088447a349ee925829f96c8e88f3d120f3
Robert Khasanov <robert.khasanov@tu-dresden.de>
2023-11-07 17:34:49 +0100

    util: operating_point: adding a cores_count field to represent the number of CPUS per each core type

--- util/operating_point.cc
+++ util/operating_point.cc
@@ -2,30 +2,22 @@
 
 namespace tetris {
 
-OperatingPoint::OperatingPoint(const OperatingPoint &old, const std::map<int,int> &conv_map)
-    : name{old.name}, characteristics{old.characteristics}, cpus{}
-{
-    for (auto cpu : old.cpus) {
-        if (conv_map.find(cpu) != conv_map.end())
-            cpus.Set(conv_map.at(cpu));
-        else
-            cpus.Set(cpu);
-    }
-}
+OperatingPoint::OperatingPoint(
+    const ClientMessage::OperatingPointsInfo::OPData &op)
+    : name{}, characteristics{}, cpus{}, cores_count{} {
+  throw std::runtime_error(
+      "Need to initialize cores_count. Either add an extra field in the "
+      "Protobuf interface, or pass a platform object in this ctor");
+  name = op.identifier();
 
-OperatingPoint::OperatingPoint(const ClientMessage::OperatingPointsInfo::OPData &op) :
-    name{}, characteristics{}, cpus{}
-{
-    name = op.identifier();
+  for (int i = 0; i < op.characteristics_size(); ++i) {
+    auto c = op.characteristics(i);
+    characteristics[c.name()] = c.value();
+  }
 
-    for (int i = 0; i < op.characteristics_size(); ++i) {
-        auto c = op.characteristics(i);
-        characteristics[c.name()] = c.value();
-    }
-
-    for (int i = 0; i < op.cpu_ids_size(); ++i) {
-        cpus.Set(op.cpu_ids(i));
-    }
+  for (int i = 0; i < op.cpu_ids_size(); ++i) {
+    cpus.Set(op.cpu_ids(i));
+  }
 }
 
 } /* namespace tetris */
