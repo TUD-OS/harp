ff4d1a21038654d8f535893e5b673d42df6d3247
Robert Khasanov <robert.khasanov@tu-dresden.de>
2023-11-08 18:09:17 +0100

    server: sched: bruteforce: optimized bruteforce scheduler
    Optimizations:
    - Check the core counters per type instead of calling allocator
    - Stop checking the branch if we cannot improve the solution

--- server/sched/bruteforce.cc
+++ server/sched/bruteforce.cc
@@ -7,12 +7,18 @@ namespace tetris {
 /**
  * Create a schedule object with the current mapping list
  */
-std::unique_ptr<Schedule> BruteforceMapper::ToSchedule(const MappingList &ops) {
+std::unique_ptr<Schedule> BruteforceMapper::ToSchedule(const MappingList &ops,
+                                                       CPUCoreSet busy_cores) {
+  auto &op_allocator = _platform.GetEquivResAllocator();
   auto schedule = std::make_unique<Schedule>(_clients, _start_time, false);
   schedule->AddSegment();
   for (int i = 0; i < _clients.size(); ++i) {
-    if (ops[i].has_value()) {
-      schedule->SetOperatingPoint(0, _clients[i], *ops[i]);
+    if (ops[i] != nullptr) {
+      auto opt_opa = op_allocator.FindEquivOP(*ops[i], busy_cores);
+      assert(opt_opa.has_value());
+      auto opa = *opt_opa;
+      schedule->SetOperatingPoint(0, _clients[i], opa);
+      busy_cores |= _platform.ToCPUCoreSet(opa.GetThreadSet());
     }
   }
   return schedule;
@@ -25,15 +31,12 @@ std::unique_ptr<Schedule> BruteforceMapper::ToSchedule(const MappingList &ops) {
  * \param best_mappings The best mappings found so far.
  * \param best_value The value of the best mappings found so far.
  */
-void BruteforceMapper::UpdateBestSolution() {
-  auto schedule = ToSchedule(_cur_ops);
-  auto cur_value = _objective->EvaluateSchedule(*schedule.get());
-  const auto [num_apps, value] = cur_value;
+void BruteforceMapper::UpdateBestSolution(int num_apps, double value) {
   const auto [best_num_apps, best_value] = _best_value;
   if ((num_apps > best_num_apps) ||
       (num_apps == best_num_apps && value < best_value)) {
-    _best_value = cur_value;
-    _best_schedule = std::move(schedule);
+    _best_value = std::make_pair(num_apps, value);
+    _best_ops = _cur_ops;
     LOGGER->debug("New best mapping: num_apps=%d, value=%f\n", num_apps, value);
   }
 }
@@ -44,29 +47,50 @@ void BruteforceMapper::UpdateBestSolution() {
  * \param n The index of the current client.
  * \param busy_cores The list of busy CPUs.
  */
-void BruteforceMapper::IterateClient(int n, CPUCoreSet busy_cores) {
+void BruteforceMapper::IterateClient(
+    int n, const std::map<std::string, int> &used_cores, int cur_apps,
+    double cur_value) {
   if (n >= _clients.size()) {
-    UpdateBestSolution();
+    UpdateBestSolution(cur_apps, cur_value);
     return;
   }
 
-  auto &op_allocator = _platform.GetEquivResAllocator();
+  // Check whether the current partial solution still can improve
+  {
+    int max_apps = cur_apps + (_clients.size() - n);
+    int best_apps = std::get<0>(_best_value);
+    double best_value = std::get<1>(_best_value);
+    if (max_apps < best_apps)
+      return;
+
+    if (max_apps == best_apps) {
+      if (cur_value > best_value)
+        return;
+    }
+  }
 
   for (auto &op : _cl_pareto[n]) {
-    // Find an equivalent mapping that is not ovelapping with busy cpus
-    auto opt_opa = op_allocator.FindEquivOP(op, busy_cores);
-    if (opt_opa.has_value()) {
-      auto opa = *opt_opa;
-      _cur_ops[n] = opa;
-      auto opa_cores = _platform.ToCPUCoreSet(opa.GetThreadSet());
-      IterateClient(n + 1, busy_cores | opa_cores);
+    // Check the operating point can be added
+    std::map<std::string, int> added_cores{used_cores};
+    bool all_fit = true;
+    for (auto &[core_type, op_core_count] : op.cores_count) {
+      added_cores[core_type] += op_core_count;
+      if (added_cores[core_type] > _platform_cores_count[core_type])
+        all_fit = false;
     }
+    if (!all_fit) {
+      continue;
+    }
+    _cur_ops[n] = &op;
+    double rem_cratio = 1.0 - _clients[n]->progress;
+    double op_value = _objective->EvaluateOP(op, rem_cratio);
+    IterateClient(n + 1, added_cores, cur_apps + 1, cur_value + op_value);
   }
 
   // Assign no mapping
   {
-    _cur_ops[n] = std::nullopt;
-    IterateClient(n + 1, busy_cores);
+    _cur_ops[n] = nullptr;
+    IterateClient(n + 1, used_cores, cur_apps, cur_value);
   }
 }
 
@@ -87,7 +111,6 @@ BruteforceMapper::GenerateSchedule(std::vector<Client *> clients,
   _start_time = start_time;
   _blocked = blocked_cores;
   _cl_pareto.clear();
-  _best_schedule = std::unique_ptr<Schedule>();
   _best_value = std::make_tuple(0, 0.0);
   _cur_ops.clear();
   _cur_ops.resize(clients.size());
@@ -101,9 +124,9 @@ BruteforceMapper::GenerateSchedule(std::vector<Client *> clients,
   }
 
   // Start bruteforce
-  IterateClient(0, _blocked);
+  IterateClient(0, _platform.GetCoreCountPerType(_blocked), 0, 0.0);
 
-  return std::move(_best_schedule);
+  return ToSchedule(_best_ops, _blocked);
 }
 
 } // namespace tetris
