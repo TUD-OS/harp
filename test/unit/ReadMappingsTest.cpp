53c7e7db26d526a82f516aeca63bd3ccc7b380e8
Robert Khasanov <robert.khasanov@tu-dresden.de>
2024-04-26 15:04:05 +0200

    test: adjusted ReadMappingDirectoryOdroidTest

--- test/unit/ReadMappingsTest.cpp
+++ test/unit/ReadMappingsTest.cpp
@@ -18,7 +18,7 @@ TEST(MappingReaderTest, ReadMappingDirectoryOdroidTest) {
   auto app_mappings = mapping_reader.read_mapping_directory(test_directory);
 
   // Check the number of applications for which mappings have been read
-  EXPECT_EQ(app_mappings.size(), 5);
+  EXPECT_EQ(app_mappings.size(), 14);
 
   // Check the number of mappings read for the 'mandelbrot' application
   EXPECT_EQ(app_mappings.count("mandelbrot"), 1);
