86969e84886270ee915380ec580987fe49b46875
Robert Khasanov <robert.khasanov@tu-dresden.de>
2024-05-10 17:08:03 +0200

    client_p: yamlMappingReader: fix a bug if the yaml files does not have any mappings

--- client_py/pytetrisclient/utils/yamlMappingReader.py
+++ client_py/pytetrisclient/utils/yamlMappingReader.py
@@ -9,18 +9,24 @@ class YamlMappingReader:
     def read_mappings(file_path, platform):
         mappings_list = []
 
-        with open(file_path, 'r') as file:
+        with open(file_path, "r") as file:
             data = yaml.safe_load(file)
             app_name = data["application"]
 
-            for mapping_data in data['mappings']:
-                name = mapping_data['name']
-                threads = mapping_data['cores']
-                cpu_ids = platform.get_affinities_of_threads(threads)
-                execution_time = mapping_data['metadata'][0]
-                energy = mapping_data['metadata'][1]
+            if data["mappings"]:
+                for mapping_data in data["mappings"]:
+                    name = mapping_data["name"]
+                    threads = mapping_data["cores"]
+                    cpu_ids = platform.get_affinities_of_threads(threads)
+                    execution_time = mapping_data["metadata"][0]
+                    energy = mapping_data["metadata"][1]
 
-                mapping = Mapping(name=name, thread_affinities=cpu_ids, exec_time=execution_time, energy=energy)
-                mappings_list.append(mapping)
+                    mapping = Mapping(
+                        name=name,
+                        thread_affinities=cpu_ids,
+                        exec_time=execution_time,
+                        energy=energy,
+                    )
+                    mappings_list.append(mapping)
 
         return mappings_list, app_name
